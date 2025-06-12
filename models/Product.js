4eea168a44bc5527646b0e231734bca1bc9ab238
Bridge Homies <homiesbridge@gmail.com>
2025-06-12 16:47:12 +0500

    Multiple Images Alllowed

--- models/Product.js
+++ models/Product.js
@@ -1,33 +1,13 @@
-// const mongoose = require('mongoose');
-
-// const ProductSchema = new mongoose.Schema({
-//   name: String,
-//   price: Number,
-//   description: String,
-//   image: String,
-//   category: String,
-//   subcategory: String,
-//   inStock: Boolean,
-//   dateAdded: String,
-//   rating: Number,
-//   reviews: Number,
-//   salePrice: {
-//     type: Number,
-//     default: null
-//   }
-// });
-
-// module.exports = mongoose.model('Product', ProductSchema);
-
+// models/Product.js
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
   name: String,
   price: Number,
   description: String,
-  image: String,
+  images: [{ type: String }],
   category: String,
-  subcategory: String,
+  subcategories: [String], 
   inStock: Boolean,
   dateAdded: { type: Date, default: Date.now },
   rating: { type: Number, default: 0 },
