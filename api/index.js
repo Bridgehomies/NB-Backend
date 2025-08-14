33852fb44b7712336e06208df21970e0e8b3d867
Bridge Homies <homiesbridge@gmail.com>
2025-08-15 00:28:40 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,15 +1,3 @@
-// api/index.js - Vercel Serverless Function for Express
-const express = require('express');
-const server = require('../server'); // points to your server.js (without app.listen)
-const { createServer } = require('http');
-
-// Remove any .listen() calls
-const app = express();
-
-// Reuse your existing middleware and routes
-app.use('/api', server); // assumes server.js exports router or app
-
-module.exports = (req, res) => {
-  // Vercel will pass incoming requests here
-  return app(req, res);
-};
+// api/index.js - Vercel serverless entry
+const app = require('../server');
+module.exports = app;
